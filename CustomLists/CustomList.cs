c8014d8184591cdef28bdb5d2a30798c87ed35e7
Hannah Bingham <binghan.hb@gmail.com>
2019-09-12 09:26:37 -0500

    Added indexer. Approved .Add Unit tests. Starting .Add method logic.

--- CustomLists/CustomList.cs
+++ CustomLists/CustomList.cs
@@ -8,8 +8,29 @@ namespace CustomLists
 {
     public class CustomList<T>
     {
+        T[] array;
+        int count;
+        public T this[int i]
+        {
+            get { return array[i]; }
+            set { array[i] = value; }
+        }
+
+
+
+        public CustomList()
+        {
+            array = new T[4];
+            count = 0;
+        }
+     
+
         public void Add(T value)
         {
+            if (count == 0)
+            {
+                
+            }
 
         }
         public void Count(T value)
--- UnitTestCustomList/UnitTest1.cs
+++ UnitTestCustomList/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace UnitTestCustomList
         {
             // arrange
             CustomList<int> Numbers = new CustomList<int>();
-            int expected = 1;
+            int expected = 2;
             int actual;
             //act
             Numbers.Add(1);
@@ -26,22 +26,31 @@ namespace UnitTestCustomList
         public void Add_AddToCustomList_AddFiveorMoreToCustomList()
         {
             CustomList<int> Numbers = new CustomList<int>();
-            int expected = 5;
+            int expected = 0;
             int actual;
 
-            Numbers.Add(0);
-            Numbers.Add(1);
-            Numbers.Add(2);
-            Numbers.Add(3);
-            Numbers.Add(4);
             Numbers.Add(5);
+            Numbers.Add(4);
+            Numbers.Add(3);
+            Numbers.Add(2);
+            Numbers.Add(1);
+            Numbers.Add(0);
             actual = Numbers[5];
 
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
+        public void Count_AddItemsToList_CountTheList()
+        {
+            CustomList<int> Numbers = new CustomList<int>();
+            int expected = 1;
+            int acutal;
 
+            Numbers.Add(2);
+            acutal = Numbers.Count;
 
+            Assert.AreEqual(expected, acutal);
+        }
     }
 }
